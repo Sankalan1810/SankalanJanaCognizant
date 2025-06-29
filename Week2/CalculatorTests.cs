c02f48d3e632eda65538b4c713adaea56d7b3959
Sankalan Jana <sankalanjana2004@gmail.com>
2025-06-29 17:09:38 +0530

    Update CalculatorTests.cs

--- Week2/CalculatorTests.cs
+++ Week2/CalculatorTests.cs
@@ -1,43 +1,45 @@
 using CalcLibrary;
+using NUnit.Framework;
 
 namespace StudentGrades.nUnitTests
 {
     [TestFixture]
     public class CalculatorTests
     {
-        private SimpleCalculator calculator;
+        private SimpleCalculator _calculator;
 
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
-           calculator = new SimpleCalculator();
+            _calculator = new SimpleCalculator();
         }
 
+        [TearDown]
         public void TearDown()
         {
-            calculator = null;
+            _calculator = null;
         }
 
-        [TestCase(90, 14,104)]
+        [TestCase(90, 14, 104)]
         [TestCase(90, 54, 144)]
-        [TestCase(50, 30, 90)]
-        [TestCase(10, 30, 20)]
+        [TestCase(50, 30, 80)] // corrected expected value
+        [TestCase(0, 0, 0)]     // added edge case
 
-        public void Addition_EqualTest(double a, double b, double expected)
-       {
-            var actual = calculator.Addition(a, b);
-            Assert.AreEqual(expected, actual);
-       }
-
-        [TestCase(50,30,90)]
-        [TestCase(10, 30, 90)]
-        [TestCase(120, 1, 121)]
-        [TestCase(50, 80, 130)]
-
-        public void Addition_NotEqualTest(double a, double b, double Notexpected)
+        public void Addition_ShouldReturnCorrectSum(double num1, double num2, double expected)
         {
-            var actual = calculator.Addition(a, b);
-            Assert.AreNotEqual(Notexpected, actual);
+            var result = _calculator.Addition(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(50, 30, 90)]
+        [TestCase(10, 30, 90)]
+        [TestCase(120, 1, 130)] // incorrect expected on purpose
+        [TestCase(5, 5, 15)]    // added case to fail if sum == 15
+
+        public void Addition_ShouldNotReturnIncorrectSum(double num1, double num2, double incorrectSum)
+        {
+            var result = _calculator.Addition(num1, num2);
+            Assert.AreNotEqual(incorrectSum, result);
         }
     }
 }
